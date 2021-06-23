76e088fc5fd3d1bcf1c94594a8f8f51b601c4e47
LIKA8 <karen.c.w.li@cargosmart.com>
2021-06-23 11:39:45 +0800

    Refactor: extra getDistinctNumbers

--- src/main/java/com/thoughtworks/refactor/PokerUtils.java
+++ src/main/java/com/thoughtworks/refactor/PokerUtils.java
@@ -47,45 +47,52 @@ public class PokerUtils {
     static String judgeHandCategory(String hand) {
         String type;
         String[] splitHands = hand.split("");
-        int[] distinctNumbers = convertHandAndSortDesc(hand);
+        int[] numbers = convertHandAndSortDesc(hand);
         int i;
         String[] suit = new String[5];
         for (i = 0; i < 5; i++) {
             suit[i] = splitHands[i * 3 + 1];
         }
-        HashSet<Integer> suits = new HashSet<>();
-        for (i = 0; i < 5; i++) {
-            suits.add(distinctNumbers[i]);
-        }
         HashSet<String> suitTypes = new HashSet<>();
         for (i = 0; i < 5; i++) {
             suitTypes.add(suit[i]);
         }
-        if (suits.size() == 5) {
-            if (distinctNumbers[0] - distinctNumbers[4] == 4 && suitTypes.size() == 1) { //五个相邻的数字且花色一样——同花顺
+        HashSet<Integer> distinctNumbers = getDistinctNumbers(numbers);
+        if (distinctNumbers.size() == 5) {
+            if (numbers[0] - numbers[4] == 4 && suitTypes.size() == 1) { //五个相邻的数字且花色一样——同花顺
                 type = "StraightFlush";
-            } else if (distinctNumbers[0] - distinctNumbers[4] == 4) { //五个相邻数字——顺子
+            } else if (numbers[0] - numbers[4] == 4) { //五个相邻数字——顺子
                 type = "Straight";
             } else if (suitTypes.size() == 1) { //同一花色——同花
                 type = "Flush";
             } else { //五个不相邻的数字——散牌
                 type = "HighCard";
             }
-        } else if (suits.size() == 4) { //一对相同，其余三个数字不同——对子
+        } else if (distinctNumbers.size() == 4) { //一对相同，其余三个数字不同——对子
             type = "OnePair";
-        } else if (suits.size() == 3) {
-            if ((distinctNumbers[0] == distinctNumbers[1] && distinctNumbers[2] == distinctNumbers[3]) || (distinctNumbers[1] == distinctNumbers[2] && distinctNumbers[3] == distinctNumbers[4]) || (distinctNumbers[0] == distinctNumbers[1] && distinctNumbers[3] == distinctNumbers[4])) { //两对
+        } else if (distinctNumbers.size() == 3) {
+            if ((numbers[0] == numbers[1] && numbers[2] == numbers[3]) || (numbers[1] == numbers[2] && numbers[3] == numbers[4]) || (numbers[0] == numbers[1] && numbers[3] == numbers[4])) { //两对
                 type = "TwoPair";
             } else { //三个数字相同，另外两个数字不同——三条
                 type = "ThreeOfAKind";
             }
         } else {
-            if (distinctNumbers[0] != distinctNumbers[1] || distinctNumbers[3] != distinctNumbers[4]) { //三个数字相同，另外两个数字相同——葫芦
+            if (numbers[0] != numbers[1] || numbers[3] != numbers[4]) { //三个数字相同，另外两个数字相同——葫芦
                 type = "FourOfAKind";
             } else { //四个数字相同——铁支
                 type = "FullHouse";
             }
         }
         return type;
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private static HashSet<Integer> getDistinctNumbers(int[] numbers) {
+        int i;
+        HashSet<Integer> distinctNumbers = new HashSet<>();
+        for (i = 0; i < 5; i++) {
+            distinctNumbers.add(numbers[i]);
+        }
+        return distinctNumbers;
     }
 }
